0746ed7fccccb15e2e913c3be0213a93689a31c5
Erick Cordeiro <erickcordeiroa@gmail.com>
2025-03-08 19:29:28 -0300

    Feat: Adicionando imagem no card do prestador

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,6 +30,7 @@ class UserFactory extends Factory
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
             'role' => $this->faker->randomElement(['user', 'provider']),
+            'avatar' => $this->faker->imageUrl()
         ];
     }
 
--- resources/js/Pages/Components/PrestadorCard.vue
+++ resources/js/Pages/Components/PrestadorCard.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="bg-white p-4 rounded-lg shadow-md">
+  <div class="bg-white p-4 rounded-lg shadow-md text-center">
+    <!-- Colocar uma imagem que vem do banco aqui -->
+    <img :src="prestador.avatar" alt="Avatar" class="w-100 rounded-lg mx-auto mb-4">
+
     <h3 class="text-xl font-semibold">{{ prestador.name }}</h3>
     <p class="text-gray-600">{{ prestador.category.name}}</p>
     <div class="mt-2">
